765fa3d7370363d5496cd9d79202906e9669f892
Arkesh Pani <paniarkesh@gmail.com>
2025-12-17 01:32:58 +0530

    Add files via upload

---
+++ CommandLinecalculator.java
@@ -0,0 +1,11 @@
+class CommandLinecalculator{
+	public static void main(String args[]){
+			int num1=Integer.parseInt(args[0]);
+	int num2=Integer.parseInt(args[1]);
+	
+	int mult=num1*num2;
+	System.out.println("mult:"+mult);
+	}
+}
+
+	
