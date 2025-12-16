678516cc3981f76cde139318ffc6b234cc627e23
Arkesh Pani <paniarkesh@gmail.com>
2025-12-17 01:31:44 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,12 @@
+public class Bitwise
+{  
+public static void main(String args[])
+{
+int a=3;
+int b=4;
+System.out.print(a&b);
+System.out.print(a^b);
+System.out.print(a|b);
+System.out.print(~a);
+}
+}	
